1db9b96c7d0edc8072f26751168c0433aee4486b
Affan Shaikh <shaikh.affan@mindbowser.com>
2024-08-11 00:40:40 +0530

    responsiveness handle.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', 'node_modules/flowbite-react/lib/esm/**/*.js'],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1920px', // Adjusted breakpoint value
+      xl: '1280px',
+    },
     extend: {
       colors: {
         'error': '#FF0000',
