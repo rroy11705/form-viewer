e94ed45eca94fb936f4cb06dc7dcf0af599097ca
smmohsinnaqvi <mohsinnaqvi.mn02@gmail.com>
2024-08-10 23:25:16 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import FormViewer from './components/common/FormViewer';
 import Matrix from './components/forms/Matrix/Matrix';
-import json from './const/PATIENT_ASSESSMENT_FORM.json';
+import json from './const/PATIENT_MEDICAL_HISTORY_FORM.json';
 
 function App() {
   return (
     <div className="h-screen">
       <FormViewer model={json} />
-      <Matrix
+      {/* <Matrix
         columns={['No', 'Yes']}
         isAllRowRequired={true}
         name={'performingActivities'}
         rows={['Eating', 'Bathing', 'Dressing', 'Walking', 'Using Toilet', 'Housekeeping']}
         label={'Do you have difficulty performing these activities by YOURSELF?'}
-      />
+      /> */}
     </div>
   );
 }
