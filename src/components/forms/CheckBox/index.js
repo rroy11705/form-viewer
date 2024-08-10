ad1c4f6925abe439c2eba4826b6651763a0ff254
rroy11705 <rroy11705@gmail.com>
2024-08-10 18:01:44 +0530

    cleanup

--- src/components/forms/CheckBox/index.js
+++ src/components/forms/CheckBox/index.js
@@ -45,7 +45,7 @@ const CheckBox = props => {
                 onBlur={onBlur}
                 disabled={disabled}
                 name="list-radio"
-                className={`w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600`}
+                className={`w-4 h-4 text-blue-600 bg-white border-gray-300 rounded focus:!ring-none  !ring-none`}
               />
               <label
                 htmlFor={elem.value}
@@ -66,7 +66,7 @@ const CheckBox = props => {
               onBlur={onBlur}
               disabled={disabled}
               name="list-radio"
-              className={`w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600`}
+              className={`w-4 h-4 text-blue-600 bg-white border-gray-300 rounded focus:!ring-none !ring-none`}
             />
             <label
               htmlFor="other"
@@ -86,7 +86,7 @@ const CheckBox = props => {
               onBlur={onBlur}
               disabled={disabled}
               name="list-radio"
-              className={`w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600`}
+              className={`w-4 h-4 text-blue-600 bg-white border-gray-300 rounded focus:!ring-none  !ring-none`}
             />
             <label
               htmlFor="none"
