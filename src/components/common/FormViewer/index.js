1a7777d153e19fa77c7631535c129b5d152ab67f
rroy11705 <rroy11705@gmail.com>
2024-08-10 14:56:56 +0530

    clean app.js

--- src/components/common/FormViewer/index.js
+++ src/components/common/FormViewer/index.js
@@ -21,7 +21,7 @@ const FormViewer = ({ model = null }) => {
 
   if (model)
     return (
-      <div className="w-full h-full">
+      <div className="w-full">
         <FormHeader title={model.title} />
         <div
           className={`w-full h-[calc(100%_-_48px)] flex ${
