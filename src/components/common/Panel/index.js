2d6ff4cd439507e48c63772e62a29f835d01aea8
rroy11705 <rroy11705@gmail.com>
2024-08-11 00:30:05 +0530

    fix

--- src/components/common/Panel/index.js
+++ src/components/common/Panel/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { renderElement } from '../../../helper';
+import { renderElement, VisibleIfRegex } from '../../../helper';
+import { useFormContext } from 'react-hook-form';
 
 const Panel = ({ element }) => {
-  return (
-    <div className="w-full bg-white border border-solid border-gray-500 rounded-md">
-      {element.title && (
-        <div className="w-full border-b border-solid border-gray-500">
-          <p className="p-4">{element.title}</p>
+  const { watch } = useFormContext();
+
+  const visibleIfMatch = element?.visibleIf?.match(VisibleIfRegex);
+  if (!element?.visibleIf || (visibleIfMatch && watch(visibleIfMatch[1]) === visibleIfMatch[2]))
+    return (
+      <div className="w-full bg-white border border-solid border-gray-500 rounded-md">
+        {element.title && (
+          <div className="w-full border-b border-solid border-gray-500">
+            <p className="p-4">{element.title}</p>
+          </div>
+        )}
+        <div className="w-full p-4 flex flex-row flex-wrap items-end gap-3">
+          {element?.elements?.map(detail => (
+            <React.Fragment key={detail.name}>{renderElement(detail)}</React.Fragment>
+          ))}
         </div>
-      )}
-      <div className="w-full p-4 flex flex-row flex-wrap items-end gap-3">
-        {element?.elements?.map(detail => (
-          <React.Fragment key={detail.name}>{renderElement(detail)}</React.Fragment>
-        ))}
       </div>
-    </div>
-  );
+    );
 };
 
 export default Panel;
